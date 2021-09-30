bf44cfea3abf3a5858357a88f6a17aec5c7d8f8a
kristjanmaidra <kristjanmaidra@yahoo.com>
2021-09-30 14:06:35 +0300

    kristjan

--- pot.py
+++ pot.py
@@ -1,21 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.selver.ee/soodushinnaga-tooted/toidukaubad?limit=94"
+URL = "https://www.selver.ee/soodushinnaga-tooted/toidukaubad?limit=1000"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-food_elements = soup.find_all("div", class_="ProductCard_link")
+food_elements = soup.find_all("h3", class_="ProductCard__title")
+# print(food_elements)
 
-Price_list = []
+price_list = []
 
 for food_el in food_elements:
     food_name = food_el.find("a").string.lower()
-    if "juust" in food_name:
-        # print(food_name)
-        if len(food_el.find_parents("span", class_="ProductBage_bage--label")) == 1:
-            price = food_el.find_parents("span", class_="ProductBage_bage--label")[0].find("label").contents[-1]
-            Price_list.append([price, food_name])
+    if "sink" in food_name:
+        print(food_name)
+        if len(food_el.find_parents("a", class_="ProductBage_bage--label")) == 1:
+            price = food_el.find_parents("a", class_="ProductBage_bage--label")[0].find("label").contents[-1]
+            price_list.append([price, food_name])
 
-print(Price_list)
+# print(price_list)
