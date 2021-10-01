e05d4ff1ec4020c433e875c6998feed8565eab10
siily <arnokosk15@gmail.com>
2021-10-01 09:22:47 +0300

    Can find prices for food

--- pot.py
+++ pot.py
@@ -1,21 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.selver.ee/soodushinnaga-tooted/toidukaubad?limit=94"
+URL = "https://www.selver.ee/liha-ja-kalatooted/varske-kala-mereannid?page=1"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-food_elements = soup.find_all("div", class_="ProductCard_link")
+food_elements = soup.find_all("div", class_="ProductCard")
+#print(food_elements)
 
-Price_list = []
+price_list = []
 
 for food_el in food_elements:
-    food_name = food_el.find("a").string.lower()
-    if "juust" in food_name:
-        # print(food_name)
-        if len(food_el.find_parents("span", class_="ProductBage_bage--label")) == 1:
-            price = food_el.find_parents("span", class_="ProductBage_bage--label")[0].find("label").contents[-1]
-            Price_list.append([price, food_name])
+    #print(food_el)
+    food_name = food_el.find("h3").string.lower()
+    if "filee" in food_name:
+        #print(food_name)
+        print(food_el.find("div", class_="ProductPrice"))
+        if len(food_el.find_parents("div", class_="ProductPrice__unit-price")) == 1:
+            price = food_el.find_parents("div", class_="ProductPrice__unit-price")[0].find("div").contents[-1]
+            price_list.append([price, food_name])
 
-print(Price_list)
+print(price_list)
